b50fe8dac75c03d970b6caa5fe548fbe34d2d221
yuying-chia <81984840+yuying-chia@users.noreply.github.com>
2021-04-10 20:34:50 +0800

    update ui

--- Shiny.R
+++ Shiny.R
@@ -1,69 +1,119 @@
 source("lastTile.R")
 source("event.R")
 source("restaurant+movement.R")
+library(shiny)
+library(shinydashboard)
 
 ui <- dashboardPage(
-  dashboardHeader(title="CARElorie"),
-  dashboardSidebar(
-    sidebarMenu(
-      #https://fontawesome.com/icons?d=gallery
-      menuItem("Welcome", tabName = "welcome", icon = icon("door-open")),
-      menuItem("Gameboard", tabName = "gameboard", icon = icon("chess-board"))
-    )
-  ),
-  dashboardBody(
-    tabItems(
-      tabItem(tabName = "welcome",
-              conditionalPanel(condition="input.start==0",
-                               "Welcome to our ESA game, where you will learn how to count calories, or starve trying!", tags$br(),
-                               actionButton("start", "Start the Game!"),
-                               actionButton("temp.lastTileTrigger", "Last Tile"),
-                               actionButton("temp.eventTrigger", label="Land on Event"),
-                               actionButton("testmenu","TestMenu"),
-                               actionButton("teststarving","TestStarving")
-              ),
-              conditionalPanel(condition = "input.start>0",
-                               imageOutput("gameboard")
-              )
-      ),
-      tabItem(tabName="gameboard",
-              h2("Turn Based Piece Movement"),
-              fluidRow(
-                box(
-                  title = "Click the die to advance a piece",width=12,
-                  # the trick here is to make the gameboard image 'position:absolute;z-order:0';
-                  # Then, to superimpose images, style them to be 'position:relative;z-order:999'
-                  img(src='FantasyMap.jpg',style="position:absolute;z-order:0",width="425px",height="415px"),
-                  imageOutput("cell11",height="100px",width="100px",click="click11",inline=TRUE), # height and width are for the containing div, not the image itself
-                  imageOutput("cell12",height="100px",width="100px",click="click12",inline=TRUE),  # height and width are for the containing div, not the image itself
-                  imageOutput("cell13",height="100px",width="100px",click="click13",inline=TRUE), # height and width are for the containing div, not the image itself
-                  imageOutput("cell14",height="100px",width="100px",click="click14",inline=TRUE),  # height and width are for the containing div, not the image itself
-                  tags$br(),
-                  imageOutput("cell21",height="100px",width="100px",click="click21",inline=TRUE), # height and width are for the containing div, not the image itself
-                  imageOutput("cell22",height="100px",width="100px",click="click22",inline=TRUE),  # height and width are for the containing div, not the image itself
-                  imageOutput("cell23",height="100px",width="100px",click="click23",inline=TRUE), # height and width are for the containing div, not the image itself
-                  imageOutput("cell24",height="100px",width="100px",click="click24",inline=TRUE),  # height and width are for the containing div, not the image itself
-                  tags$br(),
-                  imageOutput("cell31",height="100px",width="100px",click="click31",inline=TRUE), # height and width are for the containing div, not the image itself
-                  imageOutput("cell32",height="100px",width="100px",click="click32",inline=TRUE),  # height and width are for the containing div, not the image itself
-                  imageOutput("cell33",height="100px",width="100px",click="click33",inline=TRUE), # height and width are for the containing div, not the image itself
-                  imageOutput("cell34",height="100px",width="100px",click="click34",inline=TRUE),  # height and width are for the containing div, not the image itself
-                  tags$br(),
-                  imageOutput("cell41",height="100px",width="100px",click="click41",inline=TRUE), # height and width are for the containing div, not the image itself
-                  imageOutput("cell42",height="100px",width="100px",click="click42",inline=TRUE),  # height and width are for the containing div, not the image itself
-                  imageOutput("cell43",height="100px",width="100px",click="click43",inline=TRUE), # height and width are for the containing div, not the image itself
-                  imageOutput("cell44",height="100px",width="100px",click="click44",inline=TRUE),  # height and width are for the containing div, not the image itself
-                  tags$br(),
-                  tags$br(),#for more space
-                  imageOutput("die",height="410px",width="410px",click="clickdie",inline=TRUE),
-                  p("ESD Fantasy Map and Game Pieces by Tan Yi Lin"),
-                  p("Die face images by Nein Arimasen, CC BY-SA 3.0 <http://creativecommons.org/licenses/by-sa/3.0/>, via Wikimedia Commons")
-                )
-              )
-      )
-    )
-  )
-)
+    dashboardHeader(title = "CARElorie"),
+    dashboardSidebar(
+        sidebarMenu(
+            #https://fontawesome.com/icons?d=gallery
+            menuItem("Welcome", tabName = "welcome", icon = icon("door-open")),
+            menuItem("Gameboard", tabName = "gameboard", icon = icon("chess-board")),
+            menuItem("Leaderboard",tabName = "leaderboard", icon=icon("trophy"))
+        )
+        
+    ),
+    dashboardBody(
+        tabItems(
+            # First tab content
+            tabItem(tabName = "welcome",
+                    h1("Welcome, instructions"),
+                    h2("Start the game by clicking on the gameboard tab on the left"),
+                    tags$br(),
+                    selectInput("playertoken", "Select your player token:",c(1,2,3)),
+                    tags$h4("Instructions"),
+                    tags$p(" INSERT INSTRUCTIONS "),
+                    actionButton("start", "Start the Game!"),
+                    actionButton("temp.lastTileTrigger", "Last Tile"),
+                    actionButton("temp.eventTrigger", label="Land on Event"),
+                    actionButton("testmenu","TestMenu"),
+                    actionButton("teststarving","TestStarving")
+                    
+            ),
+            
+            # Second tab content
+            tabItem(tabName = "gameboard",
+                    #h2("CARElorie"),
+                    #h2("Start the game by clicking the die on the right"),
+                    fluidRow(
+                        #title = "A Very Simple Board Game",width=12,
+                        #htmlOutput("playercolorchoice"),
+                        # the trick here is to make the gameboard image 'position:absolute;z-order:0'; 
+                        # Then, to superimpose images, style them to be 'position:relative;z-order:999'
+                        
+                        sidebarLayout( 
+                            mainPanel(box(img(src='board.png',style="position:absolute;z-order:0",width="750px",height="500px"),
+                                          imageOutput("cell11",height="100px",width="100px",click="click11",inline=TRUE), # height and width are for the containing div, not the image itself
+                                          imageOutput("cell12",height="100px",width="100px",click="click12",inline=TRUE),  # height and width are for the containing div, not the image itself
+                                          imageOutput("cell13",height="100px",width="100px",click="click13",inline=TRUE), # height and width are for the containing div, not the image itself
+                                          imageOutput("cell14",height="100px",width="100px",click="click14",inline=TRUE),  # height and width are for the containing div, not the image itself
+                                          tags$br(),
+                                          imageOutput("cell21",height="100px",width="100px",click="click21",inline=TRUE), # height and width are for the containing div, not the image itself
+                                          imageOutput("cell22",height="100px",width="100px",click="click22",inline=TRUE),  # height and width are for the containing div, not the image itself
+                                          imageOutput("cell23",height="100px",width="100px",click="click23",inline=TRUE), # height and width are for the containing div, not the image itself
+                                          imageOutput("cell24",height="100px",width="100px",click="click24",inline=TRUE),  # height and width are for the containing div, not the image itself
+                                          tags$br(),
+                                          imageOutput("cell31",height="100px",width="100px",click="click31",inline=TRUE), # height and width are for the containing div, not the image itself
+                                          imageOutput("cell32",height="100px",width="100px",click="click32",inline=TRUE),  # height and width are for the containing div, not the image itself
+                                          imageOutput("cell33",height="100px",width="100px",click="click33",inline=TRUE), # height and width are for the containing div, not the image itself
+                                          imageOutput("cell34",height="100px",width="100px",click="click34",inline=TRUE),  # height and width are for the containing div, not the image itself
+                                          tags$br(),
+                                          imageOutput("cell41",height="100px",width="100px",click="click41",inline=TRUE), # height and width are for the containing div, not the image itself
+                                          imageOutput("cell42",height="100px",width="100px",click="click42",inline=TRUE),  # height and width are for the containing div, not the image itself
+                                          imageOutput("cell43",height="100px",width="100px",click="click43",inline=TRUE), # height and width are for the containing div, not the image itself
+                                          imageOutput("cell44",height="100px",width="100px",click="click44",inline=TRUE),  # height and width are for the containing div, not the image itself
+                            )),
+                            
+                            sidebarPanel( width = 4,
+                                          selectInput("r_or_e", label = "Player lands on:", c("movement","event","restaurant")),
+                                          # only show this panel if player is rolling the die
+                                          conditionalPanel(
+                                              condition = "input.r_or_e == 'movement'",
+                                              uiOutput("hunger_scale"),
+                                              imageOutput("die",height="410px",width="410px",click="clickdie",inline=TRUE)
+                                          ),
+                                          
+                                          # Only show this panel if land on restaurant tile
+                                          conditionalPanel(
+                                              condition = "input.r_or_e == 'restaurant'",
+                                              tags$h1("Menu"), # change the food names to random 5 food names from database
+                                              selectInput( inputId = "Chosenfood", label = "Please choose something:",   
+                                                           c( Filet_O_Fish = "food1", McSpicy = "food2", McChicken = "food3", CheeseBurger = "food4", Mcwrap = "food5" )
+                                              ), uiOutput("img1"), textOutput("foodingredients"), textOutput("fillinglevel"),actionButton("choosefood_yes","ok"),actionButton("choosefood_no","cancel")
+                                          )
+                                          ,
+                                          # Only show this panel if land on event
+                                          conditionalPanel(
+                                              condition = "input.r_or_e == 'event'",
+                                              fluidRow(column(4, offset = 3,
+                                                              tags$h1("Event")),
+                                                       
+                                              )
+                                              
+                                          )
+                            ))
+                        
+                    )
+            ),
+            
+            tabItem(tabName = "leaderboard", 
+                    h2("Publish Your Score"),
+                    fluidRow(
+                        box(
+                            title = "See where you stand",width=12,
+                            #actionButton("playgame", "Play the Game"),
+                            #htmlOutput("score"),
+                            #uiOutput("moreCoontrols")
+                        ))
+                    
+                    
+            )
+        )
+        
+        
+    ))
 
 server <- function(input, output, session) {
   ### Global variables (ewww)
