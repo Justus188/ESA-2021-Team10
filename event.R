c746e6278c9c431abd16ee161e15a212cdaa76c6
tpktpktpk <64466563+tpktpktpk@users.noreply.github.com>
2021-04-17 01:27:46 +0800

    Removed modals, added more helper functions

--- event.R
+++ event.R
@@ -4,24 +4,11 @@ library(DBI)
 library(tidyverse)
 
 source("lastTile.R")
-# source("setAWSPassword.R") #5FqGmSBe
-# getOption("AWSPassword")
-#
-# getAWSConnection <- function(){
-#   conn <- dbConnect(
-#     drv = RMySQL::MySQL(),
-#     dbname = "student081",
-#     host = "esddbinstance-1.ceo4ehzjeeg0.ap-southeast-1.rds.amazonaws.com",
-#     username = "student081",
-#     password = getOption("AWSPassword"))
-#   conn
-# }
-
 
 getMaxNumberOfEvents <- function(){
-  query <- "SELECT EventNumber FROM CarelorieEvents ORDER BY EventNumber DESC LIMIT 1"
+  query <- "SELECT MAX(EventNumber) FROM CarelorieEvents"
   result <- getQuery(query)
-  result[[1]]
+  as.numeric(result[[1]])
 }
 
 
@@ -47,9 +34,6 @@ getRandQuestionNo <- function(){
   query <- "SELECT QuestionNo FROM CarelorieQuestions ORDER BY RAND() LIMIT 1"
   result <- getQuery(query)
 
-  #Set the result to vals
-  vals$QuestionNo <- as.numeric(result[[1]])
-
   #Return the result
   as.numeric(result[[1]])
 }
@@ -58,6 +42,7 @@ getRandQuestionNo <- function(){
 getQuestionStatement <- function(qn_no){
   query <- str_c("SELECT QuestionStatement FROM CarelorieQuestions WHERE QuestionNo=", qn_no)
   result <- getQuery(query)
+  
   as.character(result[[1]])
 }
 
@@ -77,36 +62,34 @@ checkAnswer <- function(qn_no,selected_ans){
 }
 
 
-QuestionModal <- function(correct = FALSE, wrong=FALSE, question_no){
-  modalDialog(
-    #Show randomly generated question
-    title = "Event",
-    radioButtons(inputId="selectedAns",
-    label=getQuestionStatement(question_no),
-    choices = c("TRUE", "FALSE")),
-
-  if (correct==TRUE) {div(tags$b("Answer is Correct! Press proceed to continue"))},
-
-  if (wrong==TRUE) {div(tags$b("Answer is Incorrect! Press proceed to continue"))},
-
-  footer = tagList(
-    if(wrong==FALSE & correct==FALSE){actionButton(inputId="checkbutton", label="Check!")}else{actionButton(inputId="proceedbutton", label="Proceed!")}
-)
-)
+getQuestionExplanation <- function(qn_no){
+  query <- str_c("SELECT QuestionExplanation FROM CarelorieQuestions WHERE QuestionNo=", qn_no)
+  
+  result <- getQuery(query)
+  
+  as.character(result[[1]])
 }
 
+getEventCalorie <- function(event_no){
+  query <- str_c("SELECT Calorie FROM CarelorieEvents WHERE EventNumber=", event_no)
+  
+  result <- getQuery(query)
+  
+  as.numeric(result[[1]])
+}
 
-EventsModal <- function(event_no){
-  event_name <- getEventName(event_no)
-  event_desc <- getEventDescription(event_no)
-  image_name <- str_c('Event',event_no,'.jpg')
-  modalDialog(
-      title = event_name,
-      tags$h2(event_desc),
-      img(src=image_name,width="200px",height="200px"),
+getEventHunger <- function(event_no){
+  query <- str_c("SELECT Hunger FROM CarelorieEvents WHERE EventNumber=", event_no)
+  
+  result <- getQuery(query)
+  
+  as.numeric(result[[1]])
+}
 
-      footer = tagList(
-    actionButton(inputId="continuebutton", label="Continue")
-      )
-  )
+getEventType <- function(event_no){
+  query <- str_c("SELECT EventType FROM CarelorieEvents WHERE EventNumber=", event_no)
+  
+  result <- getQuery(query)
+  
+  as.numeric(result[[1]])
 }
